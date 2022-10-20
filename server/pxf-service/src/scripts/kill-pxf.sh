74c878d5819b0e7eeb34350a176abf52156813e4
Bradford D. Boyle <bradfordb@vmware.com>
2022-10-20 10:07:31 -0700

    Refactor OOM stop script to not use PXF CLI

--- server/pxf-service/src/scripts/kill-pxf.sh
+++ server/pxf-service/src/scripts/kill-pxf.sh
@@ -4,15 +4,40 @@ function log() {
     echo "=====> $(date) $* <======" >> "${PXF_LOGDIR}/pxf-oom.log"
 }
 
+function isRunning() {
+    ps -o pid,command -p "$1" | grep 'pxf-app' &>/dev/null
+}
+
 function _main() {
+    local PID="$1"
+    if [[ -z $PID ]]; then
+        log 'No PID provided for JVM; exiting...'
+        exit 0
+    fi
+
     sleep 1
     log 'Stopping PXF'
-    "${PXF_HOME}/bin/pxf" stop && return
-    # "insurance policy":
-    local PID=$1
-    if [[ -n $PID ]]; then
-        ps "$1" >/dev/null && kill -9 "$1"
-    fi
+    kill "${PID}" &>/dev/null
+    for i in $(seq 30); do
+        if ! isRunning "${PID}"; then
+            log "PXF stopped [${PID}]"
+            exit 0
+        elif ((i == STOP_WAIT_TIME / 2)); then
+            kill "$PID" &>/dev/null
+        fi
+        sleep 1
+    done
+
+    kill -9 "$PID" &>/dev/null
+    for i in $(seq 30); do
+        if ! isRunning "${PID}"; then
+            log "PXF stopped [${PID}]"
+            exit 0
+        elif ((i == STOP_WAIT_TIME / 2)); then
+            kill -9 "$PID" &>/dev/null
+        fi
+        sleep 1
+    done
 }
 
 log 'PXF Out of memory detected'
