17699bc68802268a2f0799188e2b7d5418b46ebd
Alexander Denissov <adenissov@vmware.com>
2022-11-09 22:44:28 -0800

    adjusted match subs

--- automation/tincrepo/main/pxf/features/hdfs/readable/avro/errors/extra_field/sql/query01.sql
+++ automation/tincrepo/main/pxf/features/hdfs/readable/avro/errors/extra_field/sql/query01.sql
@@ -1,3 +1,9 @@
 -- @description query01 for PXF HDFS Readable Avro with extra field test cases
+-- start_matchsubs
+--
+-- m/, line \d+ of/
+-- s/, line \d+ of .*//
+--
+-- end_matchsubs
 
 SELECT * from avro_extra_field ORDER BY age;
--- automation/tincrepo/main/pxf/features/hdfs/readable/avro/errors/missing_field/sql/query01.sql
+++ automation/tincrepo/main/pxf/features/hdfs/readable/avro/errors/missing_field/sql/query01.sql
@@ -1,3 +1,9 @@
 -- @description query01 for PXF HDFS Readable Avro with missing field test cases
+-- start_matchsubs
+--
+-- m/, line \d+ of/
+-- s/, line \d+ of .*//
+--
+-- end_matchsubs
 
 SELECT * from avro_missing_field;
