d2aacac7e7bd63cf0c5c2a72fa050376cb766324
Alexander Denissov <adenissov@vmware.com>
2024-03-13 15:58:19 -0700

    run with diff number of clients

--- load/workload/limit.bash
+++ load/workload/limit.bash
@@ -6,8 +6,12 @@ SCRIPT_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 
 source "${SCRIPT_DIR}/common.bash"
 
-# run limit workload only
-run_pgbench "limit_query" 60 500
+run_pgbench "limit_query" 30 1000
 
-# ramp up to 100 clients
+run_pgbench "limit_query" 40 1000
+
+run_pgbench "limit_query" 50 1000
+
+run_pgbench "limit_query" 60 1000
+
 run_pgbench "limit_query" 100 1500
