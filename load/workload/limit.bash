62865b65f9b696220342d1a9449f2fec349cd34c
Alexander Denissov <adenissov@vmware.com>
2024-03-14 16:49:05 -0700

    added low concurrency workloads

--- load/workload/limit.bash
+++ load/workload/limit.bash
@@ -6,6 +6,10 @@ SCRIPT_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 
 source "${SCRIPT_DIR}/common.bash"
 
+run_pgbench "limit_query" 10 1000
+
+run_pgbench "limit_query" 20 1000
+
 run_pgbench "limit_query" 30 1000
 
 run_pgbench "limit_query" 40 1000
