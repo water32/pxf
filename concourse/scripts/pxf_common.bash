f32a44c464ade853f059ed559a98f81e8eecaf88
Yiming Li <yimingli@vmware.com>
2024-04-02 17:27:54 -0700

    fixup! create the default server dir if it does not exit

--- concourse/scripts/pxf_common.bash
+++ concourse/scripts/pxf_common.bash
@@ -22,7 +22,7 @@ else
 	SERVER_TEMPLATES_DIR="${PXF_HOME}/templates/servers"
 	# if PXF_BASE_DIR is not set, server templates will be copied
 	# from SERVER_TEMPLATES_DIR to ${PXF_HOME}/servers/default which does not exist
-	mkdir -p "${BASE_DIR}/servers/default"
+	mkdir -p "${PXF_HOME}/servers/default"
 fi
 
 if [[ -f ~/.pxfrc ]]; then
