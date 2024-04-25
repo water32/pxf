dfd1b3242a661701a8aaee0cbe5170d725871660
Jianwen Dong <89487636+djianwen-vmware@users.noreply.github.com>
2024-04-25 13:52:56 -0500

    Use GCS to replace pivnet for GPDB artifacts (#1129)
    This commit does the following:
    - Renames the `pivnet_artifacts` pipeline to `gpdb-artifacts`. 
    - Removes `Pivnet CLI` related references from pipeline.
    - Re-writes the script to use `gsutil` with google service account. This simplifies the script a lot because we don't need to organize RPMs in multiple folders any more.
    - Removes inputs and outputs from the pipeline because we will use `gsutil cp` to directly copy RPMs from release bucket to artifact bucket.
    
    As part of this work, we also
    - Created a new bucket to replace the pivnet artifact bucket in GCP
    - Added a `gpdb-artifacts` target to the concourse Makefile

--- concourse/scripts/get_product_files.bash
+++ concourse/scripts/get_product_files.bash
@@ -2,105 +2,60 @@
 
 set -e
 
-: "${PIVNET_API_TOKEN:?PIVNET_API_TOKEN is required}"
-: "${PIVNET_CLI_DIR:?PIVNET_CLI_DIR is required}"
-: "${VERSIONS_BEFORE_LATEST:?VERSIONS_BEFORE_LATEST is required}"
-: "${LIST_OF_DIRS:?LIST_OF_DIRS is required}"
-: "${GPDB_VERSION:?GPDB_VERSION is required}"
-: "${PRODUCT_SLUG:?PRODUCT_SLUG is required}"
+: "${SOURCE_BUCKET:?SOURCE_BUCKET must be set}"
+: "${TARGET_BUCKET:?TARGET_BUCKET must be set}"
+: "${VERSIONS_BEFORE_LATEST:?VERSIONS_BEFORE_LATEST is required}" # zero-based index
+: "${GPDB_VERSION:?GPDB_VERSION is required}" # major version number, e.g. "6"
+: "${PLATFORMS:?PLATFORMS is required}" # a list of platforms separated by pipes, e.g. "el7|el8|el9"
+: "${PKG_TYPES:?PKG_TYPES is required}" # a list of package types separated by pipes, e.g. "rpm|deb"
+: "${GOOGLE_CREDENTIALS:?GOOGLE_CREDENTIALS must be set}"
 
-PATH=${PIVNET_CLI_DIR}:${PATH}
-chmod +x "${PIVNET_CLI_DIR}/pivnet"
+source_folder="gs://${SOURCE_BUCKET}/server/released/gpdb${GPDB_VERSION}"
+echo "Source GCS folder:${source_folder}"
 
-# log in to pivnet
-pivnet login "--api-token=${PIVNET_API_TOKEN}"
+target_folder="gs://${TARGET_BUCKET}/latest-${VERSIONS_BEFORE_LATEST}_gpdb${GPDB_VERSION}"
+echo "Target GCS folder: ${target_folder}"
 
-num_gpdb_versions="$(pivnet --format=json releases --product-slug="${PRODUCT_SLUG}" | jq --raw-output --argjson gpdb \""${GPDB_VERSION}"\" '[.[] | select(.version | test("^" + $gpdb))] | length')"
-echo "Found ${num_gpdb_versions} on PivNet for GPDB ${GPDB_VERSION}"
+echo "Authenticating with Google service account..."
+gcloud auth activate-service-account --key-file=<(echo "${GOOGLE_CREDENTIALS}") >/dev/null 2>&1
 
+# beta versions, release candidates, special releases, unsupported platforms will be ignored
+package_regex="greenplum-db-${GPDB_VERSION}\.[0-9]+\.[0-9]+-(${PLATFORMS})-(.*).(${PKG_TYPES})$"
+gpdb_artifact_output=$(gsutil ls "${source_folder}/" | grep -E "${package_regex}")
+
+# sort reversely (-r), with unique items (-u), and version sort (-V)
+versions=$(echo "${gpdb_artifact_output}" | grep -oE 'greenplum-db-[0-9]+\.[0-9]+\.[0-9]+' | awk -F'-' '{print $3}' | sort -ruV)
+# remove the dummy x.99.99 from the version list
+versions=$(echo "${versions}" | grep -v "${GPDB_VERSION}\.99\.99")
+
+# count the number of available versions
+num_gpdb_versions=$(echo "${versions}" | wc -l)
+echo "Number of versions: ${num_gpdb_versions}"
+
+# if `VERSIONS_BEFORE_LATEST` is out of range, use the oldest available version
 if ((VERSIONS_BEFORE_LATEST >= num_gpdb_versions)); then
-	echo "VERSION_BEFORE_LATEST (${VERSIONS_BEFORE_LATEST}) exceeds number of versions (${num_gpdb_versions}) on PivNet for Greenplum ${GPDB_VERSION}"
+	echo "VERSION_BEFORE_LATEST (${VERSIONS_BEFORE_LATEST}) exceeds number of versions (${num_gpdb_versions}) on GCS for Greenplum ${GPDB_VERSION}"
 	VERSIONS_BEFORE_LATEST=$((num_gpdb_versions - 1))
 	echo "Update VERSIONS_BEFORE_LATEST to ${VERSIONS_BEFORE_LATEST}"
 fi
 
-# get version numbers in sorted order
-# https://stackoverflow.com/questions/57071166/jq-find-the-max-in-quoted-values/57071319#57071319
-gpdb_version=$(
-	pivnet --format=json releases "--product-slug=${PRODUCT_SLUG}" |
-		jq --raw-output --argjson gpdb "\"${GPDB_VERSION}\"" --argjson m "${VERSIONS_BEFORE_LATEST}" \
-			'[.[] | select(.version | test("^" + $gpdb)) | .version] | sort_by(split(".") | map(tonumber))[-1-$m]'
-)
+# get the `VERSIONS_BEFORE_LATEST`-th (zero-based index) latest version
+target_version=$(echo "${versions}" | awk -v i=${VERSIONS_BEFORE_LATEST} 'NR == i+1')
+echo "The ${VERSIONS_BEFORE_LATEST}-th latest version is: ${target_version}"
 
-echo -e "Latest - ${VERSIONS_BEFORE_LATEST} GPDB version found:\n${GPDB_VERSION}X:\t${gpdb_version}"
+# escape the dots in the version number for regex
+version_regex="${target_version//./\\.}"
 
-# get the string of product names and turn it into list with correct gpdb_version found above
-IFS=, read -ra product_files_GPDB_VERSION <<<"${LIST_OF_PRODUCTS}"
-for file in "${product_files_GPDB_VERSION[@]}"; do
-	: "product_files/Pivotal-Greenplum/${file/GPDB_VERSION/${gpdb_version}}" # replace with version we are set to grab
-	product_files+=("${_}")
-done
+# find the packages of the version
+versioned_package_regex="greenplum-db-${version_regex}-(${PLATFORMS})-(.*).(${PKG_TYPES})$"
+packages_to_copy=$(echo "${gpdb_artifact_output}" | grep -E "${versioned_package_regex}")
 
-IFS=, read -ra product_dirs <<<"${LIST_OF_DIRS}"
+while IFS= read -r package_path; do
+  package_name=$(basename "${package_path}")
+  target_package_path="${target_folder}/${package_name}"
 
-# for this gpdb version, get the full list of product files
-product_files_json=$(pivnet --format=json product-files "--product-slug=${PRODUCT_SLUG}" --release-version "${gpdb_version}")
-for ((i = 0; i < ${#product_files[@]}; i++)); do
-	file=${product_files[$i]}
-	# find the version of GPDB already in the bucket
-	download_path=${product_dirs[$i]}/${file##*/}
-	# does the version in the bucket match what the list we pulled from pivnet?
-	[[ -e ${download_path} ]] || {
-		# if it doesn't exist, look in the previous bucket for that version
-		echo "${download_path} does not exist, looking for version Latest - $((VERSIONS_BEFORE_LATEST - 1))"
-		new_version=$((VERSIONS_BEFORE_LATEST - 1))
-		# this assumes we are naming our paths with latest-X !
-		download_path=${download_path/latest-${VERSIONS_BEFORE_LATEST}\//latest-${new_version}/}
-	}
-	# if the version of gpdb exists in the i-1 bucket but not the i-th bucket, check the SHA
-	if [[ -e ${download_path} ]]; then
-		echo "Found file ${download_path}, checking sha256sum..."
-		sha256=$(jq <<<"${product_files_json}" -r --arg object_key "${file}" '.[] | select(.aws_object_key == $object_key).sha256')
-		sum=$(sha256sum "${download_path}" | cut -d' ' -f1)
-		if [[ ${sum} == "${sha256}" ]]; then
-			echo "Sum is equivalent, skipping download of ${file}..."
-			# if the SHAs match, bring the version of gpdb in the i-1 bucket into the i-th bucket
-			if [[ ! ${download_path} =~ ^${product_dirs[$i]} ]]; then
-				echo "Cleaning ${product_dirs[$i]} and copying file ${download_path} to ${product_dirs[$i]}..."
-				rm -f "${product_dirs[$i]}"/*.{rpm,deb}
-				cp "${download_path}" "${product_dirs[$i]}"
-			fi
-			continue
-		fi
-	fi
-	# if the version doesn't exist in either i-1, i-th bucket or the SHAs don't match, download it from pivnet.
-	id=$(jq <<<"${product_files_json}" -r --arg object_key "${file}" '.[] | select(.aws_object_key == $object_key).id')
-
-	if [[ -z "${id}" ]]; then
-		echo "Did not find '${file}' in product files for GPDB '${gpdb_version}'"
-
-		case "${file}" in
-			*rhel7*) existing_file="$(find "${product_dirs[$i]}/" -name "*rhel7*.rpm")" ;;
-			*el8*) existing_file="$(find "${product_dirs[$i]}/" -name "*el8*.rpm")" ;;
-			*el9*) existing_file="$(find "${product_dirs[$i]}/" -name "*el9*.rpm")" ;;
-			*ubuntu18*) existing_file="$(find "${product_dirs[$i]}/" -name "*ubuntu18*.deb")" ;;
-			*)
-				echo "Unexpected file: ${file}"
-				exit 1
-				;;
-		esac
-
-		echo "Keeping existing file: ${existing_file}"
-		continue
-	fi
-	echo "Cleaning ${product_dirs[$i]} and downloading ${file} with id ${id} to ${product_dirs[$i]}..."
-	rm -f "${product_dirs[$i]}"/*.{rpm,deb}
-	pivnet download-product-files \
-		"--download-dir=${product_dirs[$i]}" \
-		"--product-slug=${PRODUCT_SLUG}" \
-		"--release-version=${gpdb_version}" \
-		"--product-file-id=${id}" >/dev/null 2>&1 &
-	pids+=($!)
-done
-
-wait "${pids[@]}"
+  echo "Copying GPDB release package from ${package_path} to ${target_package_path}..."
+  # gsutil automatically performs hash validation when uploading or downloading files
+  # Reference: https://cloud.google.com/storage/docs/gsutil/commands/hash
+  gsutil cp "${package_path}" "${target_package_path}"
+done <<< "${packages_to_copy}"
